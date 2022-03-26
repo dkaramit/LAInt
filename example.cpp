9f17f9c7a745e4c3bbda27a7f43b0709f7c33248
dkaramit <dkaramit@yahoo.com>
2022-03-26 21:18:35 +0000

    added a better example

--- example.cpp
+++ example.cpp
@@ -10,19 +10,39 @@
 
 using LD=double;
 
+LD BreitWigner(LD s, LD m, LD Gamma){
+	return 1/(std::pow(s-m,2) + std::pow(m*Gamma,2));
+}
+
+LD NarrowWidthApprox(std::function<LD(LD s)> csx, LD m, LD Gamma){
+	return  M_PI/(m*Gamma) *csx(4*m*m);
+}
+
 
 
 int main(){
 
-	std::function<LD(LD)> integrand=[](LD x){return std::exp(-std::pow(x-10,2)*1e5);};
+	LD m=1e3, Gamma=1e-5;
+	std::function<LD(LD)> integrand=[&m,&Gamma](LD x){
+		if(x==0){return static_cast<LD>(0);}
 
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::LHSRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::RHSRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::MidpointRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::TrapezoidalRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::SimpsonRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::GaussLegendreRule<LD>)<< "\n";
-	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,50,500),1e-9,1e-9,100,lai::GaussLobattoRule<LD>)<< "\n";
+		LD s=x/(1-x);
+		LD dsdx=1/(1-x)/(1-x);
+
+		return BreitWigner(s,m,Gamma)*dsdx;
+
+	};
+
+	/*The results should agree with this*/
+	std::cout << NarrowWidthApprox([&m,&Gamma](LD s){return 1;} , m, Gamma  )<< "\n";
+
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::LHSRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::RHSRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::MidpointRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::TrapezoidalRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::SimpsonRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::GaussLegendreRule<LD>)<< "\n";
+	std::cout << lai::LocalAdaptive<LD>::integrate(integrand,misc::linspace<LD>(0,1,60),1e-8,1e-8,50,lai::GaussLobattoRule<LD>)<< "\n";
 	
 
 	return 0;
